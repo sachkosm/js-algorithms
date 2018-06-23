cd481fa77ca1c32d9fa4ed979508f4ec0742e024
si180@yahoo.com <si180@yahoo.com>
2018-06-23 10:37:12 -0400

    'first'

---
+++ palindrom-permutation.js
@@ -0,0 +1,38 @@
+//Write a function to check if a sring is a permutation of a palindrome.
+//A palindrome is a word or phrase that is the same forwards and backwards. A permutation
+//is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+
+var palinPerm = function(string) {
+  // create object literal to store charcount
+  var chars = {};
+  var currChar;
+  var mulligan = false;
+  var isPerm = true;
+  // pump characters in, spaces not counted, all lowercase
+  string.split('').forEach((char) => {
+    if (char !== ' ') {
+      currChar = char.toLowerCase();
+      if (chars[currChar] === undefined) {
+        chars[currChar] = 0;
+      }
+      chars[currChar]++;
+    }
+  });
+  // check that all chars are even count, except for one exception
+  Object.keys(chars).forEach((char) => {
+    if (chars[char] % 2 > 0) {
+    // if more than one exception, return false
+      if (mulligan) {
+        isPerm = false; // return in a forEach statment doesn't flow out of function scope
+      } else {
+        mulligan = true;
+      }
+    }
+  });
+  // if not return true
+  return isPerm;
+};
+
+// TESTS
+console.log(palinPerm('Tact Coa'), 'true');
+console.log(palinPerm('Tact boa'), 'false');
