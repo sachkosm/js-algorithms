c3baf76cf7b447c43a944c0b124dfa2e0071e771
sachkosm <39930413+sachkosm@users.noreply.github.com>
2019-01-21 18:39:46 -0500

    Update find-substring.js

--- find-substring.js
+++ find-substring.js
@@ -1,3 +1,41 @@
+//Array subset using Map() - find if an array is a subset of another array
+const isArraySubset = (superset, subset) => {
+    if (subset.length > superset.length) {
+        return false;
+    }
+
+    const count = new Map();
+
+    for (let i = 0; i < superset.length; i++) {
+        const currentItem = superset[i]
+        if (!count.has(currentItem)) {
+            count.set(currentItem, 1)
+        } else {
+            count.set(currentItem, count.get(currentItem) + 1)
+        }
+    }
+
+    for (let i = 0; i < subset.length; i++) {
+        const currentItem = subset[i]
+
+        if (!count.has(currentItem)) {
+            return false
+        }
+
+        count.set(currentItem, count.get(currentItem) - 1)
+
+        if (count.get(currentItem) === 0) {
+            count.delete(currentItem);
+        }
+
+    }
+
+    return true
+
+}
+
+
+
 // How can you match substring of a sting?
 
 function subStringFinder(str, subStr){
